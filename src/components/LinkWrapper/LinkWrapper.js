f685cc9742484d4488622438da30ce2107716061
Natasha Hakun <natasha.hakun@ey.com>
2019-06-21 11:30:21 -0400

    Create LinkWrapper component

---
+++ src/components/LinkWrapper/LinkWrapper.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './LinkWrapper.scss';
+
+const LinkWrapper = ({ children }) => 
+    // takes in an anchor tag or a react-router Link as children
+    <div className="link-wrapper">
+        {children}
+    </div>
+
+LinkWrapper.propTypes = {
+    children: PropTypes.element.isRequired
+};
+
+export default LinkWrapper;
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { default as Button } from './Button/Button';
 export { default as Heading } from './Heading/Heading';
 export { default as Input } from './Input/Input';
 export { default as IssueTable } from './IssueTable/IssueTable';
+export { default as LinkWrapper } from './LinkWrapper/LinkWrapper';
 export { default as LoadingIndicator } from './LoadingIndicator/LoadingIndicator';
 export { default as Login } from './Login/Login';
 export { default as Notification } from './Notification/Notification';
