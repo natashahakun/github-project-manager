cda4cffe45b40b3ccc7355f00c9c9dfd1d905790
Natasha Hakun <natasha.hakun@ey.com>
2019-06-21 19:45:57 -0400

    Update App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import { mockStore } from './testConfig';
+import { Provider } from 'react-redux';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let store;
+
+beforeEach(() => {
+    store = mockStore({ 
+        ui: {
+            loading: false
+        }, 
+        user: {},
+        getState: jest.fn(() => ({})),
+        dispatch: jest.fn()
+    });
+});
+
+describe('App', () => {
+    it('renders correctly', () => {
+        const tree = renderer
+        .create(
+            <Provider store={store}>
+            <App />
+            </Provider>
+        )
+        .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 });
