ce1b31b9df2476c57649c47e03b725c482178037
Natasha Hakun <hakun.natasha@gmail.com>
2019-08-13 19:07:01 -0400

    Updates

---
+++ precache-manifest.46139374a97226ef5a5afac289f9f5f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba57b3322e49d891108c38fe1c7541e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a36e65dc10df1eaf0cc",
+    "url": "/static/css/main.b95dd3c3.chunk.css"
+  },
+  {
+    "revision": "a5de2a8755174f21a3a9",
+    "url": "/static/js/2.c8267713.chunk.js"
+  },
+  {
+    "revision": "6a36e65dc10df1eaf0cc",
+    "url": "/static/js/main.d0a7e0d0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
